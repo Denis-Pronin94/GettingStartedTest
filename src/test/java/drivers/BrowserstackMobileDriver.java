a4f4e69432deaaa72672363c7d1bbb0dc67bc3cb
dddpr <ddd.pronin@yandex.ru>
2022-06-22 21:54:28 +0300

    added project

---
+++ src/test/java/drivers/BrowserstackMobileDriver.java
@@ -0,0 +1,47 @@
+package drivers;
+
+import com.codeborne.selenide.WebDriverProvider;
+import org.aeonbits.owner.ConfigFactory;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import owner.BrowserstackAndroidConfig;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BrowserstackMobileDriver implements WebDriverProvider {
+    static BrowserstackAndroidConfig config = ConfigFactory.create(BrowserstackAndroidConfig.class);
+    
+    @Override
+    public WebDriver createDriver(Capabilities capabilities) {
+        MutableCapabilities mutableCapabilities = new MutableCapabilities();
+        mutableCapabilities.merge(capabilities);
+                // Set your access credentials
+        mutableCapabilities.setCapability("browserstack.user", config.login());
+        mutableCapabilities.setCapability("browserstack.key", config.password());
+
+        // Set URL of the application under test
+//        mutableCapabilities.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
+        mutableCapabilities.setCapability("app", config.appUrl());
+
+        // Specify device and os_version for testing
+        mutableCapabilities.setCapability("device", config.device());
+        mutableCapabilities.setCapability("os_version", config.osVersion());
+
+        // Set other BrowserStack capabilities
+        mutableCapabilities.setCapability("project", config.projectName());
+        mutableCapabilities.setCapability("build", config.buildName());
+        mutableCapabilities.setCapability("name", config.testName());
+        return new RemoteWebDriver(getBrowserstackUrl(), mutableCapabilities);
+    }
+
+    public static URL getBrowserstackUrl() {
+        try {
+            return new URL(config.baseUrl());
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
